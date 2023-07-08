636ad0471c2a3c89518ed4015d08f35a2989d7ad
Jahid Hasan <hackerphone630@gmail.com>
2023-07-08 08:20:11 +0600

    update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Route from './routes';
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-red-500 font-bold underline">
+        Hello world!
+      </h1>
+      <Route />
+    </>
+  );
+}
